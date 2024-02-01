97ae94492e1ea6e7b702798537b0d61bce4f2efa
teera19 <teera.so@rmuti.ac.th>
2024-02-01 18:52:10 +0700

    test Register

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import RegisterForm from "./layout/RegisterForm";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-4xl text-pink-600 text-center border bg-ghost-400 py-5 m-6 relative top-5m font-bold">
+        Hello world!
+      </h1>
+      <input type="checkbox" value="synthwave" className="toggle theme-controller"/>
+      <hr />
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
